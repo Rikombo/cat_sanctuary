5f9979475aca0f1463c8de5efab54b4520bd74ce
Valentins Stepanovs <valentin.stepano@gmail.com>
2023-06-23 17:05:03 +0300

    FAVS-24: created repositories, data models extend Equatable, repositories injected via flutter_bloc, code refactored with futures, search implemented

---
+++ lib/in_memory_cats_repository.dart
@@ -0,0 +1,28 @@
+import 'package:cat_sanctuary/cat_sanctuary.dart';
+import 'package:cat_sanctuary/cats_repository.dart';
+
+import 'package:cat_sanctuary/cats.dart';
+
+class InMemoryCatsRepository extends CatsRepository {
+  final List<CatSanctuary> catsData = cats;
+
+  @override
+  Future<List<CatSanctuary>> getCatOfTheDay() async {
+    final today = DateTime.now().day;
+    final index = today % cats.length;
+    return[cats[index]];
+  }
+
+  @override
+  Future<List<CatSanctuary>> getCats() async {
+    await Future.delayed(
+      const Duration(seconds: 2),
+    );
+    return catsData;
+  }
+
+  @override
+  Future<List<CatSanctuary>> searchCats(String query) async {
+    return catsData.where((catsData) => catsData.name.contains(query)).toList();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
+import 'package:cat_sanctuary/in_memory_cats_repository.dart';
 import 'package:cat_sanctuary/main_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:cat_sanctuary/cats_repository.dart';
 
 void main() {
+  final catsRepository = InMemoryCatsRepository();
+  final catsRepositoryProvider =
+      RepositoryProvider<CatsRepository>(create: (context) => catsRepository);
   runApp(
-    const MaterialApp(
-      home: MainPage(),
+    MultiRepositoryProvider(
+      providers: [catsRepositoryProvider],
+      child: const MaterialApp(
+        home: MainPage(),
+      ),
     ),
   );
 }
